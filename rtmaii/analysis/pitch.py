03b15e0c7110e1894bd5cc070fd8d7319835bc06
Andrew <Aceharris@hotmail.co.uk>
2018-01-12 13:29:54 +0000

    Cleaned up pitch module and added type hints

--- rtmaii/analysis/pitch.py
+++ rtmaii/analysis/pitch.py
@@ -1,36 +1,42 @@
 """
+    This module handles any pitch based analysis.
 
+    **INPUTS**:
+        Spectrum: a bin of frequencies and their amplitudes.
+        Signal: temporal wave form.
+
+    **OUTPUTS**:
+        Pitch (Fundamental Frequency): the pitch of the input.
+
+    **Example**:
+
+    TODO:
+        * Look into other areas i.e. harmonic product spectrum.
+        * Finish comments and documentation.
 """
-# INPUTS Spectrum, Stream
-# FUNCTIONS auto_correlation, harmonic_product_spectrum, FFT fundamental, zero_crossings
-# OUTPUTS Fundamental Frequency
-# TODO: Comment the shit out of this.
-# TODO: Look into other areas i.e. harmonic product spectrum.
-import logging
-from numpy import argmax, mean, diff, ravel
-from scipy.signal import fftconvolve
-from matplotlib.mlab import find
+from numpy import argmax, mean, diff
 
-def pitch_from_fft(spectrum, sampling_rate):
+def pitch_from_fft(spectrum: list, sampling_rate: int):
     """ Estimate pitch from the frequency spectrum """
     basic_frequency = argmax(spectrum)
     estimated_frequency = interpolate_peak(abs(spectrum), basic_frequency)
     return sampling_rate * estimated_frequency /len(spectrum)
 
-def pitch_from_auto_correlation(convolved_spectrum, sampling_rate):
+def pitch_from_auto_correlation(convolved_spectrum: list, sampling_rate: int):
     """ Estimate pitch using autocorrelation """
 
-    # Find the first low point of the fft
     spectrum_distances = diff(convolved_spectrum)
-    first_low_point = next(i for i in range(len(spectrum_distances)) if spectrum_distances[i] > 0) # Finds first rising edge
+    first_low_point = next(
+        i for i, _ in enumerate(spectrum_distances) if spectrum_distances[i] > 0
+    ) # Finds first rising edge
     peak = argmax(convolved_spectrum[first_low_point:]) + first_low_point
     interpolated_peak = interpolate_peak(convolved_spectrum, peak)
     return sampling_rate / interpolated_peak
 
-def pitch_from_zero_crossings(signal, sampling_rate):
+def pitch_from_zero_crossings(signal: list, sampling_rate: int):
     """ Estimate pitch by counting zero-crossings """
     indices = []
-    for i in range(len(signal)) : # Find indices of zero-crossings
+    for i, _ in enumerate(signal): # Find indices of zero-crossings
         if (signal[i - 1] > 0) and (signal[i] < 0):
             indices.append(i)
         elif (signal[i - 1] < 0) and (signal[i] > 0):
@@ -40,14 +46,15 @@ def pitch_from_zero_crossings(signal, sampling_rate):
 
     return sampling_rate / mean(diff(crossings))
 
-def interpolate_peak(spectrum, peak):
+def interpolate_peak(spectrum: list, peak: int):
     """
         Uses quadratic interpolation of spectral peaks to get a better estimate of the peak.
-        As the peak can sometimes1
         Based off: https://ccrma.stanford.edu/~jos/sasp/Quadratic_Interpolation_Spectral_Peaks.html
-
     """
     prev_neighbour = spectrum[peak-1]
     next_neighbour = spectrum[peak+1]
-    estimated_peak = 1/2 * (prev_neighbour  - next_neighbour / (prev_neighbour  - 2 * spectrum[peak] + next_neighbour)) # + peak 
+    estimated_peak = 1/2 * (
+        prev_neighbour  - next_neighbour /
+        (prev_neighbour  - 2 * spectrum[peak] + next_neighbour)
+    ) # + peak
     return estimated_peak
