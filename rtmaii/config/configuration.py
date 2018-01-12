5d1238e420175ef82bd7e54d6abc7c756a310116
Andrew <Aceharris@hotmail.co.uk>
2018-01-12 18:39:25 +0000

    Removed json based config and added config module

---
+++ rtmaii/config/configuration.py
@@ -0,0 +1,57 @@
+"""
+    Module for handling & storing configuring different analysis and audio settings.
+
+    TODO:
+        - Handle adding to existing settings i.e. bands of interest/removal?
+"""
+class Config(object):
+    """
+        
+    """
+    def __init__(self, **kwargs):
+
+        self.defaults = {
+            "merge_channels": False,
+            "bands": {
+                "bass":[200, 2000]
+            },
+            "fft_resolution": 10240
+        }
+
+        if kwargs is None:
+            self.settings = self.defaults
+        else:
+            self.settings = self.defaults
+            self.set_config(**kwargs)
+
+    def set_config(self, **kwargs):
+        """
+            Given a set of keyword arguments, update the config settings.
+
+            **Example**
+            ```python
+                config.set_config({
+                    'merge_channels': True,
+                    'fft_resolution': 5120
+                })
+            ```
+        """
+        for key, value in kwargs.items():
+            if hasattr(self.settings, key):
+                self.settings[key] = value
+            else:
+                raise KeyError("{} is not a valid configuration setting".format(key))
+
+    def get_config(self, setting):
+        """
+            Retreive a setting from the config object.
+        """
+        return self.settings[setting]
+
+    def set_source(self, args):
+        """
+            Change the audio source settings.
+        """
+        self.settings['pyaudio_settings'] = args
+        self.settings['sampling_rate'] = args['rate']
+        self.settings['channels'] = args['channels']
