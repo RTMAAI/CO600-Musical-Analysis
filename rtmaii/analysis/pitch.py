4d1b7dbc0784144dc286d5cb179728c34373c744
Andrew <Aceharris@hotmail.co.uk>
2018-01-12 12:06:15 +0000

    Updated pitch analysis script with further comments.

--- rtmaii/analysis/pitch.py
+++ rtmaii/analysis/pitch.py
@@ -14,20 +14,18 @@ from matplotlib.mlab import find
 def pitch_from_fft(spectrum, sampling_rate):
     """ Estimate pitch from the frequency spectrum """
     basic_frequency = argmax(spectrum)
-    true_frequency = parabolic(abs(spectrum), basic_frequency)[0]
-    return sampling_rate * true_frequency /len(spectrum)
+    estimated_frequency = interpolate_peak(abs(spectrum), basic_frequency)
+    return sampling_rate * estimated_frequency /len(spectrum)
 
 def pitch_from_auto_correlation(convolved_spectrum, sampling_rate):
     """ Estimate pitch using autocorrelation """
 
     # Find the first low point of the fft
-    d = diff(convolved_spectrum)
-    first_low_point = next(i for i in range(len(d)) if d[i] > 0) # Finds first rising edge
-
+    spectrum_distances = diff(convolved_spectrum)
+    first_low_point = next(i for i in range(len(spectrum_distances)) if spectrum_distances[i] > 0) # Finds first rising edge
     peak = argmax(convolved_spectrum[first_low_point:]) + first_low_point
-    px, py = parabolic(convolved_spectrum, peak)
-
-    return sampling_rate / px
+    interpolated_peak = interpolate_peak(convolved_spectrum, peak)
+    return sampling_rate / interpolated_peak
 
 def pitch_from_zero_crossings(signal, sampling_rate):
     """ Estimate pitch by counting zero-crossings """
@@ -42,8 +40,14 @@ def pitch_from_zero_crossings(signal, sampling_rate):
 
     return sampling_rate / mean(diff(crossings))
 
-def parabolic(f, x):
-    """ """
-    xv = 1/2 * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
-    yv = f[x] - 1/4 * (f[x-1] - f[x+1]) * (xv - x)
-    return (xv, yv)
+def interpolate_peak(spectrum, peak):
+    """
+        Uses quadratic interpolation of spectral peaks to get a better estimate of the peak.
+        As the peak can sometimes1
+        Based off: https://ccrma.stanford.edu/~jos/sasp/Quadratic_Interpolation_Spectral_Peaks.html
+
+    """
+    prev_neighbour = spectrum[peak-1]
+    next_neighbour = spectrum[peak+1]
+    estimated_peak = 1/2 * (prev_neighbour  - next_neighbour / (prev_neighbour  - 2 * spectrum[peak] + next_neighbour)) # + peak 
+    return estimated_peak
