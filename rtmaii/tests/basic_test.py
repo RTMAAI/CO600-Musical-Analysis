8cbe3e63b8bbe8b5b53cc65723e1d6d382e6b779
Andrew <Aceharris@hotmail.co.uk>
2018-01-18 14:03:18 +0000

    renamed test folder to tests

---
+++ rtmaii/tests/basic_test.py
@@ -0,0 +1,14 @@
+'''
+    Test file
+    Sine Wave, Sawtooth and Square
+'''
+import unittest
+import pyaudio
+from rtmaii.analysis import pitch
+
+class TestSuite(unittest.TestCase):
+    '''
+        Test Suite
+    '''
+    def test(self):
+        self.assertEqual(1, 1)
