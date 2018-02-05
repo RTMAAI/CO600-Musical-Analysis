7e794e1fa75548ffc523c0b21354c412496bc44c
Andrew <Aceharris@hotmail.co.uk>
2018-02-05 20:22:08 +0000

    Debugger layout improvements

--- debugger.py
+++ debugger.py
@@ -24,6 +24,7 @@ CHUNK_LENGTH = 1024 # Length of sampled data
 SPECTRUM_LENGTH = int(CHUNK_LENGTH/2) # Only need half of a spectrum to get all frequencies present.
 SAMPLING_RATE = 44100 # Default sampling rate 44.1 khz
 FRAME_DELAY = 200 # How long between each frame update (ms)
+XPADDING = 20
 
 class Listener(threading.Thread):
     """ Starts analysis and holds a state of analysed results.
@@ -44,7 +45,6 @@ class Listener(threading.Thread):
             callbacks.append({'function': self.callback, 'signal': key})
 
         self.analyser = rtmaii.Rtmaii(callbacks,
-                                      track=r'.\test_data\sine_493.88.wav',
                                       mode='CRITICAL',
                                      )
 
@@ -53,7 +53,7 @@ class Listener(threading.Thread):
         self.start()
 
     def start_analysis(self):
-        """ Start analysis """
+        """ Start analysis. """
         self.analyser.start()
 
     def stop_analysis(self):
@@ -93,57 +93,73 @@ class Debugger(tk.Tk):
         """Create UI elements and assign configurable elements. """
         # --- INIT SETUP --- #
         self.timeframe = arange(0, CHUNK_LENGTH) # split x axis up to 1
+        self.title("RTMAII DEBUGGER")
+        #self.pack_propagate(0) # Disables auto-resizing of elements.
+
+        # --- CONTROL FRAME --- #
+        control_frame = tk.Frame(self, borderwidth=1)
+        control_frame.pack(side=tk.TOP)
 
         # --- CONTROLS --- #
-        play = tk.Button(self, text="PLAY", command=self.listener.start_analysis)
-        play.pack(padx=10, side=tk.LEFT)
+        play = tk.Button(control_frame, text="PLAY", command=self.listener.start_analysis)
+        play.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
 
-        stop = tk.Button(self, text="STOP", command=self.listener.stop_analysis)
-        stop.pack(padx=10, side=tk.RIGHT)
+        stop = tk.Button(control_frame, text="STOP", command=self.listener.stop_analysis)
+        stop.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
 
-        # --- BASE LABEL --- #
-        channel_label = tk.Label(self, text=str('channel 1'))
-        channel_label.pack()
+        # --- GRAPH FRAME---- #
+        graph_frame = tk.Frame(self, borderwidth=1, width=500, height=500)
+        graph_frame.pack(side=tk.LEFT)
 
         # --- SIGNAL GRAPH --- #
-        signal_frame = Figure(figsize=(5, 5), dpi=30)
+        signal_frame = Figure(figsize=(5, 5), dpi=100)
         self.signal_plot = signal_frame.add_subplot(111)
         self.signal_plot.plot(self.timeframe, self.timeframe)
         self.signal_plot.set_ylim([100, 20000])
-        self.signal_canvas = FigureCanvasTkAgg(signal_frame, self)
+        self.signal_canvas = FigureCanvasTkAgg(signal_frame, graph_frame)
         self.signal_canvas.show()
-        self.signal_canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.signal_canvas.get_tk_widget().pack()
 
         # --- SPECTRUM GRAPH --- #
         self.frequencies = arange(SPECTRUM_LENGTH)/(CHUNK_LENGTH/SAMPLING_RATE) # Possible range of frequencies
-        spectrum_frame = Figure(figsize=(15, 5), dpi=100)
+        spectrum_frame = Figure(figsize=(5, 5), dpi=100)
         self.spectrum_plot = spectrum_frame.add_subplot(111)
         self.spectrum_plot.plot(self.frequencies, self.frequencies)
         self.spectrum_plot.set_xlim([0, 20000]) # TODO Fix this
-        self.spectrum_canvas = FigureCanvasTkAgg(spectrum_frame, self)
+        self.spectrum_canvas = FigureCanvasTkAgg(spectrum_frame, graph_frame)
         self.spectrum_canvas.show()
-        self.spectrum_canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.spectrum_canvas.get_tk_widget().pack()
+
+        # --- VALUE FRAME --- #
+        value_frame = tk.LabelFrame(self, borderwidth=1, width=500, height=500, text="Analysed Values")
+        value_frame.pack(side=tk.RIGHT)
 
         # --- PITCH LABEL --- #
         self.pitch = tk.StringVar()
-        pitch_label = tk.Label(self, text=str('Pitch'))
-        pitch_label.pack(padx=5, side=tk.LEFT)
-        pitch_value = tk.Label(self, textvariable=self.pitch)
-        pitch_value.pack(padx=5, side=tk.LEFT)
+        pitch_frame = tk.Frame(value_frame, borderwidth=1)
+        pitch_frame.pack(padx=10)
+        pitch_label = tk.Label(pitch_frame, text=str('Pitch:'))
+        pitch_label.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
+        pitch_value = tk.Label(pitch_frame, textvariable=self.pitch)
+        pitch_value.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
 
         # --- KEY LABEL --- #
         self.key = tk.StringVar()
-        key_label = tk.Label(self, text=str('Key'))
-        key_label.pack()
-        key_value = tk.Label(self, textvariable=self.key)
-        key_value.pack()
+        key_frame = tk.Frame(value_frame, borderwidth=1)
+        key_frame.pack(padx=10)
+        key_label = tk.Label(key_frame, text=str('Key:'))
+        key_label.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
+        key_value = tk.Label(key_frame, textvariable=self.key)
+        key_value.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
 
         # --- BANDS LABEL --- #
         self.bands = tk.StringVar()
-        bands_label = tk.Label(self, text=str('Bands'))
-        bands_label.pack()
-        bands_label = tk.Label(self, textvariable=self.bands)
-        bands_label.pack()
+        pitch_frame = tk.Frame(value_frame, borderwidth=1)
+        pitch_frame.pack(padx=10)
+        bands_label = tk.Label(pitch_frame, text=str('Bands:'))
+        bands_label.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
+        bands_label = tk.Label(pitch_frame, textvariable=self.bands)
+        bands_label.pack(padx=XPADDING, fill=tk.X, side=tk.LEFT)
 
     def update(self):
         """ Update UI every FRAME_DELAY milliseconds """
