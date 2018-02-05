ba406e4245d54805c6d70cec01caf2c2e593c529
Andrew <Aceharris@hotmail.co.uk>
2018-02-05 10:48:42 +0000

    Temp hotfix for key

--- rtmaii/analysis/key.py
+++ rtmaii/analysis/key.py
@@ -5,7 +5,11 @@ note_strings = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A"
 def note_from_pitch(frequency):
     """
         12 Possible notes * (Natural log)
+        Fn = F0 * (a)**n
+
+        TODO: update comments
+        TODO: fix issue with frequency being recorded as 0hz
     """
-    note_num = 12 * (log(frequency / 440)/log(2))
+    note_num = frequency if frequency == 0 else 12 * (log(frequency / 440)/log(2))
     note_index = int(round(note_num)+69)%12
     return note_strings[note_index]
