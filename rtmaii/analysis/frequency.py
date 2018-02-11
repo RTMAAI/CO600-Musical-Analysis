5b171e85578350c9debe6627fe02a2de746f80d5
Andrew <Aceharris@hotmail.co.uk>
2018-02-11 17:59:01 +0000

    Added some more comments and moved summation of frequency powers to different method to allow easier testing.

--- rtmaii/analysis/frequency.py
+++ rtmaii/analysis/frequency.py
@@ -19,16 +19,27 @@ def normalize_dict(dictionary: dict, dict_sum: float):
     return { key: float(value)/dict_sum for key, value in dictionary.items() }
 
 
+def get_band_power(spectrum: list, bands: dict):
+    """ Get the summed power of each frequency range provided by bands.
+
+        **Args**:
+            - spectrum: the spectrum to be summed against.
+            - bands: the bands to sum powers of.
+    """
+    return {band: sum(spectrum[int(values[0]):int(values[1])])
+                     for band, values in bands.items()}
+
 def frequency_bands(spectrum: list, bands: dict):
     """
         Creates a Dictionary of the amplitude balance between each input frequency band.
+
+        **Args**:
+            - spectrum: the spectrum to analyse.
+            - bands: the band ranges to find the presence of.
     """
     filtered_spectrum = remove_noise(spectrum, 0.5)
-
-    band_presence = {band: sum(filtered_spectrum[int(values[0]):int(values[1])])
-                     for band, values in bands.items()}
-
-    normalized_presence = normalize_dict(band_presence, sum(filtered_spectrum))
+    band_power = get_band_power(spectrum, bands)
+    normalized_presence = normalize_dict(band_power, sum(filtered_spectrum))
 
     return normalized_presence
 
