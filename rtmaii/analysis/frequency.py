4623b15e5c227aa55e55fb9b0eb43e9910340551
Andrew <Aceharris@hotmail.co.uk>
2018-02-11 17:16:04 +0000

    Changed normalization function to take a sum
    This is so that in cases where only one band is selected in config, the presence of that band will be compared against the sum of the entire frequency spectrum.

--- rtmaii/analysis/frequency.py
+++ rtmaii/analysis/frequency.py
@@ -1,28 +1,34 @@
+"""
+    Frequency analysis module.
+    - Analyses parts of the frequency spectrum, including the presence of each band.
+"""
 # INPUTS spectrum
 # OUTPUTS frequency_bands
-# TODO: Comment the shit out of this.
 
-def remove_noise(spectrum, noise_level):
-    """ Remove any frequencies with an amplitude under the noise_level param when calculating balance """
+def remove_noise(spectrum: list, noise_level: float):
+    """ Remove any frequencies with an amplitude under the noise_level param when calculating balance. """
     return list(map(lambda amp: 0 if amp < noise_level else amp, spectrum))
 
-def normalize_dict(dictionary):
-    """ Constrain dictionary values to continous range 0-1. """
-    dict_sum = sum(dictionary.values())
-    norm = { key: float(value)/dict_sum for key, value in dictionary.items() }
-    return norm
+def normalize_dict(dictionary: dict, dict_sum: float):
+    """ Constrain dictionary values to continous range 0-1 based on the dictionary sum given.
 
-def frequency_bands(spectrum, bands):
+        **Args**:
+            - dictionary: the dictionary to be normalized.
+            - dict_sum: the sum value to normalize with.
+    """
+    return { key: float(value)/dict_sum for key, value in dictionary.items() }
+
+
+def frequency_bands(spectrum: list, bands: dict):
     """
         Creates a Dictionary of the amplitude balance between each input frequency band.
-        The sum of all values adds up to 1.
     """
     filtered_spectrum = remove_noise(spectrum, 0.5)
 
     band_presence = {band: sum(filtered_spectrum[int(values[0]):int(values[1])])
                      for band, values in bands.items()}
 
-    normalized_presence = normalize_dict(band_presence)
+    normalized_presence = normalize_dict(band_presence, sum(filtered_spectrum))
 
     return normalized_presence
 
