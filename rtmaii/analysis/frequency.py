16caafdf59e18786785dda0f9935928d4095edcb
Andrew <Aceharris@hotmail.co.uk>
2018-02-11 17:06:15 +0000

    Frequency bands presence now normalized to 0-1 range, based on their presence compared to other bands.

--- rtmaii/analysis/frequency.py
+++ rtmaii/analysis/frequency.py
@@ -6,9 +6,11 @@ def remove_noise(spectrum, noise_level):
     """ Remove any frequencies with an amplitude under the noise_level param when calculating balance """
     return list(map(lambda amp: 0 if amp < noise_level else amp, spectrum))
 
-def constrain_bands(bands):
-    """ Constrain Frequency bands to sum to 1 """
-    pass
+def normalize_dict(dictionary):
+    """ Constrain dictionary values to continous range 0-1. """
+    dict_sum = sum(dictionary.values())
+    norm = { key: float(value)/dict_sum for key, value in dictionary.items() }
+    return norm
 
 def frequency_bands(spectrum, bands):
     """
@@ -16,12 +18,13 @@ def frequency_bands(spectrum, bands):
         The sum of all values adds up to 1.
     """
     filtered_spectrum = remove_noise(spectrum, 0.5)
-    band_width = 19980 # Should be configurable
-    # TODO: Scale values to add up to 1, remove noise i.e. frequencies with minute amplitudes
+
     band_presence = {band: sum(filtered_spectrum[int(values[0]):int(values[1])])
                      for band, values in bands.items()}
-    return band_presence
+
+    normalized_presence = normalize_dict(band_presence)
+
+    return normalized_presence
 
 # NOTE: Could analyse other frequencies in spectrum, find overtones and harmonics
 # Default configuration should be bass, lows, highs
-# Possible add tracker for a set frequency yourself. configurable bands.
