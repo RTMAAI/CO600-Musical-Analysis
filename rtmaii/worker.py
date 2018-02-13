61547814530fd18568e4fcd944ca0ac20cf0ff5d
RRaule <32111218+RRaule@users.noreply.github.com>
2018-02-13 21:34:47 +0000

    Created SpectogramWorker Class and imported more numpy functions: olumn_stack, power, log10, absolute

--- rtmaii/worker.py
+++ rtmaii/worker.py
@@ -2,7 +2,9 @@ import threading
 from queue import Queue
 from rtmaii.analysis import frequency, pitch, key, spectral, spectrogram
 from pydispatch import dispatcher
-from numpy import arange, mean, int16, resize
+from numpy import arange, mean, int16, resize, column_stack, power, log10, absolute
+from matplotlib import pyplot as plt
+
 
 class Worker(threading.Thread):
     """ Base worker class, responsible for initializing shared attributes.
@@ -29,6 +31,55 @@ class PitchWorker(Worker):
     def analyse_key(self, pitch):
         estimated_key = key.note_from_pitch(pitch)
         dispatcher.send(signal='key', sender=self.channel_id, data=estimated_key)
+
+class SpectogramWorker(Worker):
+    """ Worker responsible for creating spectograms ... .
+
+        **Args**:
+            - `sampling_rate`: sampling_rate of source being analysed.
+            - `channel_id`: id of channel being analysed.
+    """
+    def __init__(self, sampling_rate: int, channel_id: int):
+        Worker.__init__(self, channel_id)
+        self.sampling_rate = sampling_rate
+
+    def run(self):
+        while True:
+            ffts = self.queue.get()
+            if ffts is None:
+                break # No more data so cleanup and end thread.
+
+            fft_resolution = 20480
+            
+            self.window = len(spectral.new_window(fft_resolution, 'blackmanharris'))
+            
+            ffts = column_stack(ffts)
+            ffts = absolute(ffts) * 2.0 / self.window
+            ffts = ffts / power(2.0, 8* 2 - 1)
+            ffts = (20 * log10(ffts)).clip(-120)
+
+            time = arange(0, ffts.shape[1], dtype=float) * self.window / self.sampling_rate / 2
+            frequecy = arange(0, self.window / 2, dtype=float) * self.sampling_rate / self.window
+            smallerFFTS = []
+            smallerF = []
+
+            for i in range(0, len(ffts), 80):
+                if i + 80 > len(ffts):
+                    break
+
+                meanF = 0
+                meanFFTS = 0
+
+                for j in range(i,i+80):
+                    meanF = meanF + frequecy[j] 
+                    meanFFTS = meanFFTS + ffts[j]
+
+                smallerF.append(meanF)
+                smallerFFTS.append(meanFFTS)
+
+            spectroData = [time, smallerF, smallerF]
+            dispatcher.send(signal='spectogram', sender=self.channel_id, data=spectroData)
+
 
 class BandsWorker(Worker):
     """ Worker responsible for analysing interesting frequency bands.
