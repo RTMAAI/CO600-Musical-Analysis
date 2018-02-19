c51abefe03c3a0c0d95a1e83dde349a2dca4da94
Andrew <Aceharris@hotmail.co.uk>
2018-02-19 14:22:27 +0000

    Workers/Coordinators no check for none data
    Workers and Coordinators are daemons so if root node is destroyed, children will be destroyed too.

--- rtmaii/worker.py
+++ rtmaii/worker.py
@@ -44,9 +44,6 @@ class BandsWorker(Worker):
     def run(self):
         while True:
             spectrum = self.queue.get()
-            if spectrum is None:
-                break # No more data so cleanup and end thread.
-
             frequency_bands = frequency.frequency_bands(abs(spectrum), self.bands_of_interest)
             dispatcher.send(signal='bands', sender=self.channel_id, data=frequency_bands) #TODO: Move to a locator.
 
@@ -64,9 +61,6 @@ class ZeroCrossingWorker(PitchWorker):
     def run(self):
         while True:
             signal = self.queue.get()
-            if signal is None:
-                break # No more data so cleanup and end thread.
-
             estimated_pitch = pitch.pitch_from_zero_crossings(signal, self.sampling_rate)
             dispatcher.send(signal='pitch', sender=self.channel_id, data=estimated_pitch)
             self.analyse_key(estimated_pitch)
@@ -85,9 +79,6 @@ class AutoCorrelationWorker(PitchWorker):
     def run(self):
         while True:
             signal = self.queue.get()
-            if signal is None:
-                break # No more data so cleanup and end thread.
-
             convolved_spectrum = spectral.convolve_spectrum(signal)
             estimated_pitch = pitch.pitch_from_auto_correlation(convolved_spectrum, self.sampling_rate)
             dispatcher.send(signal='pitch', sender=self.channel_id, data=estimated_pitch)
@@ -108,9 +99,6 @@ class HPSWorker(PitchWorker):
     def run(self):
         while True:
             spectrum = self.queue.get()
-            if spectrum is None:
-                break # No more data so cleanup and end thread.
-
             estimated_pitch = pitch.pitch_from_hps(spectrum, self.sampling_rate, 7)
             dispatcher.send(signal='pitch', sender=self.channel_id, data=estimated_pitch)
             self.analyse_key(estimated_pitch)
@@ -129,9 +117,6 @@ class FFTWorker(PitchWorker):
     def run(self):
         while True:
             spectrum = self.queue.get()
-            if spectrum is None:
-                break # No more data so cleanup and end thread.
-
             estimated_pitch = pitch.pitch_from_fft(spectrum, self.sampling_rate)
             dispatcher.send(signal='pitch', sender=self.channel_id, data=estimated_pitch)
             self.analyse_key(estimated_pitch)
