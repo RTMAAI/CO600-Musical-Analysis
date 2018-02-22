e087e8153819ac9c343daa5ad6c8b34a2f33b410
Andrew <Aceharris@hotmail.co.uk>
2018-02-22 16:38:39 +0000

    Added another test to double check bands analysis.

--- rtmaii/test/basic/test_basic_bands.py
+++ rtmaii/test/basic/test_basic_bands.py
@@ -14,7 +14,7 @@ class TestSuite(unittest.TestCase):
     def setUp(self):
         """ Perform setup of initial parameters. """
         self.band_sum = 10000
-        self.bands = 10230
+        self.interested_bands = {'low': [0 , 10], 'med': [10, 70], 'high': [70, 100]}
         self.spectrum = arange(0, 100, 1) # Create 100 values increasing by 1 at each step.
         self.bands = {'0.1': 10, '0.2': 20, '0.3': 30, '1': 100} # key value where key == expected value after normalization.
         self.bands_sum = 100 # Sum to compare normalized values against.
@@ -37,6 +37,12 @@ class TestSuite(unittest.TestCase):
         """ Test that a given frequency range is summed correctly. """
         power = frequency.get_band_power(self.spectrum, {'full_range': [0, len(self.spectrum)]})
         self.assertEqual(power['full_range'], sum(self.spectrum))
+
+    def test_multiple_band_power(self):
+        """ Test that multiple frequency ranges are summed correctly. """
+        power = frequency.get_band_power(self.spectrum, self.interested_bands)
+        for key, value in self.interested_bands.items():
+            self.assertEqual(power[key], sum(self.spectrum[value[0]:value[1]]))
 
     def test_frequency_bands(self):
         """ End-to-end test of retrieiving the presence of a frequency bands. """
