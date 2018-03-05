6f52d93b564bb5d1386a44f6530fb649896a76ce
RRaule <32111218+RRaule@users.noreply.github.com>
2018-03-05 20:05:44 +0000

    Added prediction functionaility to spectogram worker

--- rtmaii/worker.py
+++ rtmaii/worker.py
@@ -2,8 +2,9 @@ import threading
 from rtmaii.workqueue import WorkQueue
 from rtmaii.analysis import frequency, pitch, key, spectral
 from pydispatch import dispatcher
-from numpy import arange, mean, int16, resize, column_stack, power, log10, absolute
+from numpy import arange, mean, int16, resize, column_stack, power, log10, absolute, reshape
 from matplotlib import pyplot as plt
+from tensorflow.contrib import predictor
 
 
 class Worker(threading.Thread):
@@ -43,6 +44,13 @@ class SpectrogramWorker(Worker):
     def __init__(self, sampling_rate: int, channel_id: int):
         Worker.__init__(self, channel_id)
         self.sampling_rate = sampling_rate
+        self.predict_fn = predictor.from_saved_model(r'C:\Users\RalphRaulePC\Documents\FinalYearProject\CO600-Musical-Analysis\rtmaii\model\model\\')
+        self.dict = {}
+        self.dict[1] = 'Electronic'
+        self.dict[2] = 'Folk'
+        self.dict[3] = 'Hip-Hop'
+        self.dict[4] = 'Pop'
+        self.dict[5] = 'Rock'
 
     def run(self):
         while True:
@@ -53,6 +61,7 @@ class SpectrogramWorker(Worker):
             self.window = 1024
             
             ffts = column_stack(ffts)
+            print(ffts.shape)
             ffts = absolute(ffts) * 2.0 / self.window
             ffts = ffts / power(2.0, 8* 2 - 1)
             ffts = (20 * log10(ffts)).clip(-120)
@@ -79,6 +88,16 @@ class SpectrogramWorker(Worker):
 
                 smallerF.append(meanF)
                 smallerFFTS.append(meanFFTS)
+            
+            #print(smallerFFTS.shape)
+
+            testPhoto = reshape(smallerFFTS, (1,128,128,1))
+            predictions = self.predict_fn({"x": testPhoto})
+            predictionClass = predictions['classes'][0]
+            
+            prediction = self.dict[predictionClass]
+
+            print(predictions['probabilities'])
 
             #ax = plt.subplot(111)
             #plt.pcolormesh(time, smallerF, smallerF, vmin=-120, vmax=0)
