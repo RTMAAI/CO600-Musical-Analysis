a1dbd0e4e3be09965e42ef5257cc1ba15a65b700
LBaeriswyl <lvb6@kent.ac.uk>
2018-03-10 13:43:34 +0000

    Corrected name typo in setup.py
    Baeriswyl was misspelled in setup.py so I've quickly changed it.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='Rtmaii',
     version='0.1.0',
-    author='A. Harris, L. Baerisqwyl, R. Raule',
+    author='A. Harris, L. Baeriswyl, R. Raule',
     packages=['rtmaii'],
     license='LICENSE.txt',
     description='Realtime Musical Analysis and AI Package',
