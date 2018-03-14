1f835c131cf4f1b15fc2be55076d4fdee2255487
Andrew <Aceharris@hotmail.co.uk>
2018-03-14 14:34:53 +0000

    Cleaned up key module

--- rtmaii/analysis/key.py
+++ rtmaii/analysis/key.py
@@ -1,26 +1,45 @@
+""" KEY MODULE
+
+    This module contains any methods relating to extracting the key/note of a frequency.
+
+"""
 from math import log
 
-note_strings = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"]
+ROOT_NOTES = ["C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab", "A", "A#/Bb", "B"]
 
-def note_from_pitch(frequency):
-    """
-        12 Possible notes * (Natural log)
-        Fn = F0 * (a)**n
-    """
-    estimated_num = note(frequency)
-    offset = cents_off(frequency, estimated_num)
-    note_index = int(estimated_num % 12)
-    return {'key': note_strings[note_index], 'cents_off': offset}
+def note_from_pitch(frequency: float) -> dict:
+    """ Returns the root note and cents off of a given frequency.
 
-def note(frequency):
-    note_num = frequency if frequency == 0 else 12 * (log(frequency / 440)/log(2))
-    note_index = round(note_num) + 69
-    return note_index
+        **Args**:
+            - `frequency`: Frequency (Hz) to find the root note of.
 
-def cents_off(frequency, note_index):
     """
-        Get how far off the estimated key the note was.
+    midi_num = get_midi_num(frequency)
+    offset = get_cents_off(frequency, midi_num)
+    note = ROOT_NOTES[round(midi_num % 12)] # Remainder = note index position in NOTE_STRINGS.
+    return {'note': note, 'cents_off': offset}
+
+def get_midi_num(frequency: float) -> float:
+    """ Finds the closest midi note on a piano given a frequency in Hertz.
+
+        Uses mathematical equations listed here: https://newt.phys.unsw.edu.au/jw/notes.html
+
+        **Args**:
+            - `frequency`: Frequency to compare against.
     """
-    frequency_of_note = 440 * (2 ** ((note_index - 69) / 12))
-    cents_off = int(1200 * log(frequency / frequency_of_note, 2))
+    semitones_off_a4 = frequency if frequency == 0 else 12 * (log(frequency / 440)/log(2))
+    midi_num = semitones_off_a4 + 69
+    return midi_num
+
+def get_cents_off(frequency: float, midi_num: float) -> float:
+    """ Get how far off the estimated note the frequency is.
+
+        Uses mathematical equations listed here: https://en.wikipedia.org/wiki/Cent_(music)
+
+        **Args**:
+            - `frequency`: Detected frequency.
+            - `midi_num`: Midi num to compare against.
+    """
+    midi_note_frequency = 440 * (2 ** ((midi_num - 69) / 12))
+    cents_off = round(1200 * log(frequency / midi_note_frequency, 2))
     return cents_off
