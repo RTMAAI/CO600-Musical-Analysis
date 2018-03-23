7b59b4579b57550499353678c8f4ce41fbfb72af
Andrew <Aceharris@hotmail.co.uk>
2018-03-23 18:01:22 +0000

    Further cleanup of analysis modules for linter

--- rtmaii/analysis/pitch.py
+++ rtmaii/analysis/pitch.py
@@ -1,11 +1,11 @@
 """ PITCH MODULE
     This module handles any pitch based analysis.
 
-    **INPUTS**:
+    INPUTS:
         Spectrum: a bin of frequencies and their amplitudes.
         Signal: temporal wave form.
 
-    **OUTPUTS**:
+    OUTPUTS:
         Pitch (Fundamental Frequency): the pitch of the input.
 """
 from numpy import argmax, mean, diff, real
@@ -14,14 +14,14 @@ from scipy.signal import decimate
 def pitch_from_fft(spectrum: list, sampling_rate: int) -> float:
     """ Estimate pitch from the frequency spectrum.
 
-        **Args**:
+        Args:
             - spectrum: the frequency bin to analyze.
             - sampling_rate: the sampling rate of the audio source.
 
-        *Advantages*:
+        Advantages:
             - More accurate than Zero-crossings.
 
-        *Disadvantages*:
+        Disadvantages:
             - Not great at detecting pitch with multiple harmonics that
               have a higher amplitude than the fundamental frequency.
     """
@@ -32,15 +32,15 @@ def pitch_from_fft(spectrum: list, sampling_rate: int) -> float:
 def pitch_from_auto_correlation(convolved_signal: list, sampling_rate: int) -> float:
     """ Estimate pitch using the autocorrelation method.
 
-        **Args**:
+        Args:
             - convolved_spectrum: the convolved frequency bin to analyze.
             - sampling_rate: the sampling rate of the audio source.
 
-        *Advantages*:
+        Advantages
             - Good for repetitive wave forms, i.e. sine waves/saw tooths.
             - Represents a pitch closer to what humans hear.
 
-        *Disadvantages*:
+        Disadvantages:
             - Requires an FFT which can be expensive.
             - Not great with inharmonics i.e. Guitars/Pianos.
     """
@@ -55,29 +55,30 @@ def pitch_from_auto_correlation(convolved_signal: list, sampling_rate: int) -> f
 def pitch_from_zero_crossings(signal: list, sampling_rate: int) -> float:
     """ Estimate pitch by simply counting the amount of zero-crossings.
 
-        **Args**:
+        Args:
             - signal: the signal bin to analyze.
             - sampling_rate: the sampling rate of the audio source.
 
-        *Advantages*:
+        Advantages:
             - Good for intermittent stable frequencies, i.e. Guitar Tuners.
             - Fast to compute, don't need to apply an FFT.
 
-        *Disadvantages*:
+        Disadvantages:
             - If there is lots of noise or multiple frequencies doesn't work.
     """
     indices = []
     for i, _ in enumerate(signal): # Find indices of zero-crossings
         if (signal[i - 1] > 0) and (signal[i] < 0):
             indices.append(i)
-    crossings = [i - signal[i] / (signal[i + 1] - signal[i]) for i in indices] # Linear interpolation.
+    # Linear interpolation, gives a more accurate result, to a few decimal places.
+    crossings = [i - signal[i] / (signal[i + 1] - signal[i]) for i in indices]
 
     return sampling_rate / mean(diff(crossings)) # Convert to Hz
 
 def pitch_from_hps(spectrum: list, sampling_rate: int, max_harmonics: int) -> float:
     """ Estimate pitch using the harmonic product spectrum (HPS) Algorithm
 
-        **Args**:
+        Args:
             - spectrum: the frequency bin to analyze.
             - sampling_rate: the sampling rate of the audio source.
             - max_framonics the sampling rate of the audio source.
@@ -101,7 +102,7 @@ def pitch_from_hps(spectrum: list, sampling_rate: int, max_harmonics: int) -> fl
 def interpolate_peak(spectrum: list, peak: int) -> float:
     """ Uses quadratic interpolation of spectral peaks to get a better estimate of the peak.
 
-        **Args**:
+        Args:
             - spectrum: the frequency bin to analyze.
             - peak: the location of the estimated peak into the spectrum list.
 
@@ -110,5 +111,6 @@ def interpolate_peak(spectrum: list, peak: int) -> float:
     prev_neighbour = spectrum[peak-1]
     next_neighbour = spectrum[peak+1]
     peak_value = spectrum[peak]
-    estimated_peak = (next_neighbour - prev_neighbour) / (2 * peak_value - prev_neighbour - next_neighbour) + peak
+    estimated_peak = (next_neighbour
+                      - prev_neighbour) / (2 * peak_value - prev_neighbour - next_neighbour) + peak
     return real(estimated_peak)
--- rtmaii/analysis/spectral.py
+++ rtmaii/analysis/spectral.py
@@ -1,23 +1,24 @@
 """ SPECTRAL MODULE
     This module handles temporal to spectral signal conversion.
 
-    **INPUTS**:
+    INPUTS:
         Signal: Temporal wave form.
 
-    **OUTPUTS**:
+    OUTPUTS:
         Spectrum: Frequency spectrum of the input sample.
 """
 from scipy.signal import butter, lfilter, fftconvolve, get_window
 from scipy.fftpack import fft
 
-def butter_bandpass(low_cut_off: int, high_cut_off: int, sampling_rate: int, order: int = 5) -> dict:
+def butter_bandpass(low_cut_off: int, high_cut_off: int,
+                    sampling_rate: int, order: int = 5) -> dict:
     """ Cut out any frequencies out of the range we are interested in.
 
-        **Args**
-            - `low_cut_off`: lower end of bandpass filter.
-            - `high_cut_off`: upper end of bandpass filter.
-            - `sampling_rate`: sampling rate of the signal being analysed.
-            - `order`: magnitude of the filter created.
+        Args
+            - low_cut_off: lower end of bandpass filter.
+            - high_cut_off: upper end of bandpass filter.
+            - sampling_rate: sampling rate of the signal being analysed.
+            - order: magnitude of the filter created.
     """
     nyquist_frequency = 0.5 * sampling_rate
     low = low_cut_off / nyquist_frequency
@@ -28,10 +29,10 @@ def butter_bandpass(low_cut_off: int, high_cut_off: int, sampling_rate: int, ord
 def band_pass_filter(signal: list, numerator: list, denominator: list) -> list:
     """ Cut out any frequencies out of the range we are interested in.
 
-        **Args**
-            - `signal`: length of window to create.
-            - `numerator`: numerator of filter.
-            - `denominator`: denominator of filter.
+        Args
+            - signal: length of window to create.
+            - numerator: numerator of filter.
+            - denominator: denominator of filter.
     """
     filtered_signal = lfilter(numerator, denominator, signal)
     return filtered_signal
@@ -39,9 +40,9 @@ def band_pass_filter(signal: list, numerator: list, denominator: list) -> list:
 def new_window(window_length: int, window: str) -> list:
     """ Generate a new smoothing window for use.
 
-        **Args**
-            - `window_length`: length of window to create.
-            - `window`: the smoothing window to be applied.
+        Args
+            - window_length: length of window to create.
+            - window: the smoothing window to be applied.
     """
     window = get_window(window, window_length, True)
     return window
@@ -49,8 +50,8 @@ def new_window(window_length: int, window: str) -> list:
 def convolve_signal(signal: list) -> list:
     """ Apply convolution to the input signal.
 
-        **Args**
-            - `signal`: the signal to convolve.
+        Args
+            - signal: the signal to convolve.
     """
     convol = fftconvolve(signal, signal[::-1], mode='full')
     return convol[len(convol) // 2:] # Split bin in half removing negative lags.
@@ -58,8 +59,8 @@ def convolve_signal(signal: list) -> list:
 def spectrum_transform(signal: list) -> list:
     """ Performs FFT on input signal, returns only positive half of spectrum.
 
-        **Args**
-            - `signal`: the signal to perform a fourier transform on.
+        Args
+            - signal: the signal to perform a fourier transform on.
     """
     signal_length = len(signal)
     normalized_spectrum = fft(signal) / signal_length # Normalization
@@ -70,15 +71,16 @@ def spectrum(signal: list,
              bp_filter: dict = None) -> list:
     """ Return the frequency spectrum of an input signal.
 
-        **Args**
-            - `signal`: the temporal signal to be converted to a spectrum.
-            - `window`: the smoothing window to be applied.
-            - `bp_filter`: the bandpass filter polynomial coefficents to apply to the signal.
+        Args
+            - signal: the temporal signal to be converted to a spectrum.
+            - window: the smoothing window to be applied.
+            - bp_filter: the bandpass filter polynomial coefficents to apply to the signal.
                 In the form of {'numerator': list, 'denominator': list}
     """
     windowed_signal = signal * window
-    filtered_signal = windowed_signal if bp_filter is None else band_pass_filter(windowed_signal,
-                                                                                 bp_filter['numerator'],
-                                                                                 bp_filter['denominator'])
+    filtered_signal = windowed_signal if bp_filter is None else band_pass_filter(
+        windowed_signal,
+        bp_filter['numerator'],
+        bp_filter['denominator'])
     frequency_spectrum = spectrum_transform(filtered_signal)
     return frequency_spectrum
