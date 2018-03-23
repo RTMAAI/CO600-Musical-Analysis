69b14fe95a4fac1e545a0802e4d395baf47a1396
Andrew <Aceharris@hotmail.co.uk>
2018-03-23 21:48:10 +0000

    Removed asterisks and backticks from comments of workqueue

--- rtmaii/workqueue.py
+++ rtmaii/workqueue.py
@@ -9,16 +9,19 @@ from threading import Condition
 class WorkQueue(object):
     """ Used by workers and coordinators to manage their internal work queue.
 
-        **Attributes**:
-            - `condition`: Queue Lock, allowing threads to wait until they are notified.
-            - `queue`: Queue of data to be processed.
+        Attributes:
+            - condition: Queue Lock, allowing threads to wait until they are notified.
+            - queue: Queue of data to be processed.
     """
     def __init__(self, queue_length: int = None):
         self.condition = Condition()
         self.queue = deque([], queue_length) if queue_length else deque()
 
     def get(self) -> object:
-        """ Get last added item from work queue. If empty sleep thread. """
+        """ Get last added item from work queue. If empty block until item available.
+
+            When blocking, the process will sleep until a condition is sent.
+        """
         if not self.queue: # Wait until a notification is sent.
             with self.condition:
                 self.condition.wait()
@@ -28,7 +31,7 @@ class WorkQueue(object):
     def get_all(self) -> list:
         """ Get all items currently present in work queue extending the original object.
 
-            If queue is empty `blocks` until an item is available.
+            If queue is empty this blocks until an item is available.
         """
         data = []
         if not self.queue: # Wait until a notification is sent.
@@ -41,8 +44,8 @@ class WorkQueue(object):
     def put(self, data: object):
         """ Put item onto the work queue and send a notification that new item has been added.
 
-            **Args**
-                - `data`: data to be added.
+            Args
+                - data: data to be added.
         """
         with self.condition:
             self.queue.append(data)
