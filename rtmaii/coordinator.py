6bbc017910d7ae77fad2915b5c601d5503912b0d
Andrew <Aceharris@hotmail.co.uk>
2018-03-23 21:16:21 +0000

    Cleaned up coordinator comments, removing asterisks.

--- rtmaii/coordinator.py
+++ rtmaii/coordinator.py
@@ -15,15 +15,14 @@ LOGGER = logging.getLogger()
 class Coordinator(threading.Thread):
     """ Parent class of all coordinator threads.
 
-        Conducts the initiliazation of coordinator threads and their *required* attributes.
+        Conducts the initiliazation of coordinator threads and their attributes.
 
-        **Attributes**:
-            - `queue` (Queue): Coordinators queue of data to be processed.
-            - `peer_list` (list): List of peer threads to communicate processed data with.
-            - `channel_id`: id of channel being analysed.
-            - `config` (Config): Configuration object of library to fetch analysis values from.
-            - `queue_length` (Int): Maximum length of a coordinator's queue, helps to cull items.
-
+        Attributes:
+            - queue (WorkQueue): Coordinators queue of data to be processed.
+            - peer_list (list): List of peer threads to communicate processed data with.
+            - channel_id (int): id of channel being analysed.
+            - config (Config): Configuration object of library to fetch analysis values from.
+            - queue_length (int): Maximum length of a coordinator's queue, helps to cull items.
     """
     def __init__(self, config: object = None, channel_id: int = None, queue_length: int = None):
         threading.Thread.__init__(self, args=(), kwargs=None)
@@ -42,26 +41,25 @@ class Coordinator(threading.Thread):
     def message_peers(self, data: object):
         """ Sends input data to each peered thread.
 
-            **Args**:
-                - `data`: The data to send to each peer.
+            Args:
+                - data: The data to send to each peer.
         """
         for peer in self.peer_list:
             peer.queue.put(data)
 
     def reset_attributes(self):
-        """ Inherited method, used for resetting any attributes on configuration changes. """
+        """ Inherited method, override to reset attributes on configuration changes. """
         pass
 
-    def add_peer(self, thread_obj):
+    def add_peer(self, thread_obj: str):
         """ Add a thread to the peer_list
 
             Args:
                 - thread_obj: thread to add.
-
         """
         self.peer_list.append(thread_obj)
 
-    def remove_peer(self, thread_id):
+    def remove_peer(self, thread_id: str):
         """ Remove a thread from the peer_list
 
             Args:
@@ -69,8 +67,8 @@ class Coordinator(threading.Thread):
         """
         self.peer_list.remove(thread_id)
 
-    def get_peer_list(self):
-        """ Get peer list of thread. """
+    def get_peer_list(self) -> list:
+        """ Returns peer list of the coordinator. """
         return self.peer_list
 
 class RootCoordinator(Coordinator):
@@ -79,8 +77,11 @@ class RootCoordinator(Coordinator):
         If multi channel analysis is enabled, the root coordinator will message each,
         channel hierarchy with their own channel data, otherwise the data is merged.
 
-        **Attributes**:
-            - `config` (Config): Configuration options to use in analysis.
+        Attributes:
+            - config (obj): Configuration object to fetch analysis settings from. (Inherited)
+            - merge_channels (bool): Merge channel data by averaging.
+            - channels (int): number of channels of the audio source being analysed.
+            - frame_size (int): size of frames received in each sample.
     """
     def __init__(self, **kwargs: dict):
         LOGGER.info('Coordinator Initialized.')
@@ -117,42 +118,47 @@ class RootCoordinator(Coordinator):
 class FrequencyCoordinator(Coordinator):
     """ Frequency coordinator responsible for extending signal data before further analysis.
 
-        **Attributes**:
-            - `channel_id` (int): The ID of the channel being analysed.
-            - `peer_list` (list): List of peer threads to communicate processed data with.
-            - `config` (Config): Configuration object of library to fetch analysis values from.
+        Attributes:
+            - channel_id (int): The ID of the channel being analysed. (Inherited)
+            - peer_list (list): List of peer threads to communicate processed data with. (Inherited)
+            - config (obj): Configuration object to fetch analysis settings from. (Inherited)
+            - extended_signal (list): Aggregated signal samples over time.
+            - frequency_resolution (int): Threshold of extended_signal length, before messaging.
 
-        **Notes**:
-            - `Peers` created are dependent on configured tasks and algorithms.
+        Notes:
+            - Peers created are dependent on configured tasks and algorithms.
     """
     def __init__(self, **kwargs: dict):
         Coordinator.__init__(self, kwargs['config'], kwargs['channel_id'])
-        self.signal = []
+        self.extended_signal = []
 
     def reset_attributes(self):
         """ Reset object attributes, to latest config values. """
         self.frequency_resolution = self.config.get_config('frequency_resolution')
-        self.signal = []
+        self.extended_signal = []
 
     def run(self):
         """ Extend signal data to configured resolution before transmitting to peers. """
         while True:
             data = self.queue.get_all()
-            self.signal.extend(data)
-            self.signal = self.signal[-self.frequency_resolution:]
-            if len(self.signal) >= self.frequency_resolution:
-                self.message_peers(self.signal)
+            self.extended_signal.extend(data)
+            self.extended_signal = self.extended_signal[-self.frequency_resolution:]
+            if len(self.extended_signal) >= self.frequency_resolution:
+                self.message_peers(self.extended_signal)
 
 class SpectrumCoordinator(Coordinator):
     """ Spectrum coordinator responsible for creating spectrum data and transmitting to dependants.
 
-        **Attributes**:
-            - `channel_id` (int): The ID of the channel being analysed.
-            - `peer_list` (list): List of peer threads to communicate processed data with.
-            - `config` (Config): Configuration options to use.
+        Attributes:
+            - channel_id (int): The ID of the channel being analysed. (Inherited)
+            - peer_list (list): List of peer threads to communicate processed data with. (Inherited)
+            - config (obj): Configuration object to fetch analysis settings from. (Inherited)
+            - sampling_rate (int): Sampling rate of audio source (Hz)
+            - window (list): pre-processing smoothing window to apply to signal.
+            - filter (dict): pre-processing filter coefficients to use against signal.
 
-        **Notes**:
-            - `Peers` created are dependent on configured tasks and algorithms.
+        Notes:
+            - Peers created are dependent on configured tasks and algorithms.
     """
     def __init__(self, **kwargs: dict):
         Coordinator.__init__(self, kwargs['config'], kwargs['channel_id'], 1)
@@ -165,6 +171,7 @@ class SpectrumCoordinator(Coordinator):
         self.filter = spectral.butter_bandpass(60, 18000, self.sampling_rate, 5)
 
     def run(self):
+        """ Convert input signal into it's frequency spectrum equivalent. """
         while True:
             signal = self.queue.get()
             frequency_spectrum = spectral.spectrum(signal, self.window, self.filter)
@@ -217,9 +224,9 @@ class FFTSCoordinator(Coordinator):
 class SpectrogramCoordinator(Coordinator):
     """ Coordinator responsible for creating spectograms ... .
 
-        **Args**:
-            - `sampling_rate`: sampling_rate of source being analysed.
-            - `channel_id`: id of channel being analysed.
+        Args:
+            - sampling_rate: sampling_rate of source being analysed.
+            - channel_id: id of channel being analysed.
     """
     def __init__(self, **kwargs: dict):
         Coordinator.__init__(self, kwargs['config'], kwargs['channel_id'])
