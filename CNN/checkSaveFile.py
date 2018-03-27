1ad3296eba94bb7c8299bef711c8cd5585665356
RRaule <32111218+RRaule@users.noreply.github.com>
2018-03-27 15:46:14 +0100

    Added CNN model to library

--- CNN/checkSaveFile.py
+++ CNN/checkSaveFile.py
@@ -2,21 +2,13 @@ import pickle
 import numpy as np
 from numpy.linalg import norm
 
-with open(r"C:\Users\RalphRaulePC\Documents\FinalYearProject\CO600-Musical-Analysis\CNN\save.p", "rb") as input_file:
-    debuggerSpectrograms = pickle.load(input_file)
+with open(r"./save.p", "rb") as input_file:
+    savedSpectrograms = pickle.load(input_file)
 
-#with open(r"C:\Users\RalphRaulePC\Documents\FinalYearProject\CO600-Musical-Analysis\CNN\TestClassify\OwnRockSpectrogram", "rb") as input_file:
-#    trainingSpectrograms = pickle.load(input_file)
-
-differenceArray = []
-
-print("size of debbugger:", len(debuggerSpectrograms))
-#print("size of training:", len(trainingSpectrograms))
-
-#differenceArray = []
+print("Number of Labelled spectrograms:", len(debuggerSpectrograms))
 
 debuggerSpectrograms = np.array(debuggerSpectrograms)
-#trainingSpectrograms = np.array(trainingSpectrograms)
+
 print(debuggerSpectrograms[2][0][0])
 #print(trainingSpectrograms[2][0][0])
 
@@ -26,8 +18,3 @@ for i in range (0, 1000):
     #print ("what", i)
     #print(np.subtract(debuggerSpectrograms[i][0][0], trainingSpectrograms[i][0][0]))
 
-
-
-
-#for i in e:
-#    print(i)
